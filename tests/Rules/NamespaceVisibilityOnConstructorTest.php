f6df7fc35ebc3f3cda33dac9f5e82d7d3d5e081c
Dave Liddament <dave@lampbristol.com>
2023-05-11 18:02:59 +0100

    REFACTOR move similar tests to the same class

--- tests/Rules/NamespaceVisibilityOnConstructorTest.php
+++ tests/Rules/NamespaceVisibilityOnConstructorTest.php
@@ -19,7 +19,7 @@ class NamespaceVisibilityOnConstructorTest extends AbstractNamespaceVisibilityRu
         );
     }
 
-    public function testMethodCall(): void
+    public function testConstructorCall(): void
     {
         $this->assertErrorsReported(
             __DIR__.'/data/namespaceVisibility/namespaceVisibilityOnConstructor.php',
@@ -31,6 +31,43 @@ class NamespaceVisibilityOnConstructorTest extends AbstractNamespaceVisibilityRu
                     true,
                 ],
             ],
+        );
+    }
+
+    public function testExcludeSubNamespacePositionalArgumentUsed(): void
+    {
+        $this->assertErrorsReported(
+            __DIR__.'/data/namespaceVisibility/namespaceVisibilityOnConstructorExcludeSubNamespacesPositional.php',
+            [
+                [
+                    40,
+                    'NamespaceVisibilityOnConstructorExcludeSubNamespacesPositional\Person::__construct',
+                    'NamespaceVisibilityOnConstructorExcludeSubNamespacesPositional',
+                    false,
+                ],
+            ],
+        );
+    }
+
+    public function testExcludeSubNamespaceNamedArgumentUsed(): void
+    {
+        $this->assertErrorsReported(
+            __DIR__.'/data/namespaceVisibility/namespaceVisibilityOnConstructorExcludeSubNamespaces.php',
+            [
+                [
+                    40,
+                    'NamespaceVisibilityOnConstructorExcludeSubNamespaces\Person::__construct',
+                    'NamespaceVisibilityOnConstructorExcludeSubNamespaces',
+                    false,
+                ],
+            ],
+        );
+    }
+
+    public function testDifferentNamespace(): void
+    {
+        $this->assertNoErrorsReported(
+            __DIR__.'/data/namespaceVisibility/namespaceVisibilityOnConstructorForDifferentNamespaces.php',
         );
     }
 }
